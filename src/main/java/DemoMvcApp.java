e1953f2e115f21ba1603789cf12b08d943f2a91c
Yaroslav Sydorenko <yaroslav.leo@gmail.com>
2021-01-05 00:18:00 +0200

    Add basic resources for simple mvc

---
+++ src/main/java/DemoMvcApp.java
@@ -0,0 +1,6 @@
+public class DemoMvcApp {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/controller/HomeController.java
@@ -0,0 +1,15 @@
+package controller;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping(method = GET, path = "/")
+    public String home() {
+        return "home";
+    }
+}
